72982edd6fc1a3af1f6f5bb5175e2be95758c548
successdevop <successraphael28@gmail.com>
2026-03-24 17:56:48 +0100

    I made modifications to how the account number and PIN is being generated to guarantee uniqueness

--- service.py
+++ service.py
@@ -43,11 +43,15 @@ def generate_account_number() -> str:
     10 random numbers stored in a list, which makes the customer unique account number
     :return: a string of 10 random numbers
     """
-    numbers = []
-    while len(numbers) < 10:
-        numbers.append(random.randint(1, 9))
+    # numbers = []
+    # while len(numbers) < 10:
+    #     numbers.append(random.randint(1, 9))
+    # return "".join(str(num) for num in numbers)
 
-    return "".join(str(num) for num in numbers)
+    while True:
+        acc = "".join(str(random.randint(1, 9)) for _ in range(10))
+        if not any(user["account_number"] == acc for user in users):
+            return acc
 
 
 def validate_name(prompt) -> str:
@@ -69,11 +73,14 @@ def generate_pin() -> str:
     4 random numbers stored in a list, which makes the customer unique pin
     :return: a string of 4 random numbers
     """
-    pin = []
-    while len(pin) < 4:
-        pin.append(random.randint(1, 9))
-
-    return "".join(str(num) for num in pin)
+    # pin = []
+    # while len(pin) < 4:
+    #     pin.append(random.randint(1, 9))
+    # return "".join(str(num) for num in pin)
+    while True:
+        pin = "".join(str(random.randint(1, 9)) for _ in range(4))
+        if not any(u["pin"] == pin for u in users):
+            return pin
 
 
 def validate_amount_input(prompt) -> int:
@@ -243,3 +250,40 @@ def top_3_richest_account():
     for top_3 in reversed(top_3_accounts):
         print(f"Name: {top_3["name"]} - Balance: #{top_3["balance"]}")
 
+
+def main():
+    while True:
+        print("1. Create Account")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Transfer")
+        print("5. Balance")
+        print("6. History")
+        print("7. Top_3_account")
+        print("0. Exit")
+
+        choice = input("> ")
+
+        if choice == "1":
+            create_account()
+        elif choice == "2":
+            deposit()
+        elif choice == "3":
+            withdraw()
+        elif choice == "4":
+            transfer()
+        elif choice == "5":
+            check_balance()
+        elif choice == "6":
+            transaction_history()
+        elif choice == "7":
+            top_3_richest_account()
+        elif choice == "0":
+            break
+        else:
+            print("Invalid number")
+        print(users)
+        print()
+
+
+main()

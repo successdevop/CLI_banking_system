349cd06365b8a366cd4564385b893214866f85a6
successdevop <successraphael28@gmail.com>
2026-03-24 13:47:14 +0100

    created the deposit function which help users to deposit money into their account after validating all user inputs

--- service.py
+++ service.py
@@ -1,9 +1,23 @@
 import random
 
-users = []
+users = [{
+        "name": "John",
+        "account_number": "1234567890",
+        "balance": 5000,
+        "pin": "4444",
+        "transactions": [
+            ("deposit", 2000),
+            ("withdraw", 1000)
+        ]
+    }]
 
 
 def generate_account_number() -> str:
+    """
+    this function generates account number for users. It uses the while loop to generate
+    10 random numbers stored in a list, which makes the customer unique account number
+    :return: a string of 10 random numbers
+    """
     numbers = []
     while len(numbers) < 10:
         numbers.append(random.randint(1, 9))
@@ -11,7 +25,12 @@ def generate_account_number() -> str:
     return "".join(str(num) for num in numbers)
 
 
-def validate_name(prompt):
+def validate_name(prompt) -> str:
+    """
+    this function validates the user's name
+    :param prompt: it takes the user input from the CLI
+    :return: a string that must be or greater than three characters
+    """
     while True:
         name = input(prompt).strip()
         if name and len(name) >= 3:
@@ -20,6 +39,11 @@ def validate_name(prompt):
 
 
 def generate_pin() -> str:
+    """
+    this function generates pin for users. It uses the while loop to generate
+    4 random numbers stored in a list, which makes the customer unique pin
+    :return: a string of 4 random numbers
+    """
     pin = []
     while len(pin) < 4:
         pin.append(random.randint(1, 9))
@@ -27,7 +51,53 @@ def generate_pin() -> str:
     return "".join(str(num) for num in pin)
 
 
+def validate_amount_input(prompt) -> float:
+    """
+    this function takes the user input and checks if it is an actually number
+    and that the number is also not negative
+    :param prompt: user input
+    :return: float number
+    """
+    amount = float(input(prompt))
+    try:
+        if amount > 0:
+            return float(amount)
+    except ValueError:
+        print("Invalid amount: (amount cannot be negative/empty)")
+
+
+def find_user() -> dict:
+    """
+    this function searches for a user in our user's list. If the user exist, it returns
+    the user otherwise it prints user not found
+    :return: a user dictionary
+    """
+    data = {}
+
+    while True:
+        pin = input("Enter your PIN: ")
+        account_number = input("Enter your account number: ")
+
+        if pin.isnumeric() and account_number.isnumeric():
+            if len(pin) == 4 and len(account_number) == 10:
+                data["pin"] = pin
+                data["account_number"] = account_number
+                break
+        print("Invalid PIN or account number")
+        print()
+
+    for user in users:
+        if (data["pin"] == user["pin"]) and (data["account_number"] == user["account_number"]):
+            data = user
+            return data
+    print("user not found")
+
+
 def create_account():
+    """
+    this function creates an account for users and store the information
+    in our user's list
+    """
     name = validate_name("Please enter your name: ")
     account_number = generate_account_number()
     pin = generate_pin()
@@ -36,5 +106,15 @@ def create_account():
     users.append(data)
 
 
-create_account()
-print(users)
+def deposit():
+    """
+    this function takes the user amount and adds it to the user available balance
+    after validating all user inputs
+    :return: None
+    """
+    if len(users) == 0:
+        return
+
+    user = find_user()
+    amount = validate_amount_input("Enter deposit amount: ")
+    user["balance"] += amount
